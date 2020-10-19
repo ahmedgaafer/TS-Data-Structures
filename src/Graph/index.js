d2c78bcf1db66c6e7aacd7607dffa132eb8ccf26
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2020-10-19 22:21:16 +0200

    Update index.js

--- src/Graph/index.js
+++ src/Graph/index.js
@@ -3,6 +3,13 @@ const withTry = require('../HigherOrderFunctions/withTry');
 function Graph(isUniDirectional = false, isWeighted= false){
     this.graph = {};
     this.vertcies = 0;
+    this.unidirectional = isUniDirectional;
+    this.weighted = isWeighted;
 }
+
+Graph.prototype.foo = withTry(function(){});
+Graph.prototype.foo = withTry(function(){});
+Graph.prototype.foo = withTry(function(){});
+Graph.prototype.foo = withTry(function(){});
 
 module.exports = Graph;
