a61aaaf4e20737f98fdf7a94a759ff7d4ab212fb
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2020-10-19 21:37:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const SinglyLinkedList = require('./src/SinglyLinkedList/index');
 const DoublyLinkedList = require('./src/DoublyLinkedList/index');
 const Queue            = require('./src/Queue/index');
 const Stack            = require('./src/Stack/index');
+const Heap             = require('./src/Heap/index');
 
 
 module.exports = {
@@ -9,4 +10,5 @@ module.exports = {
      DoublyLinkedList,
      Queue,
      Stack,
+     Heap,
     }   
