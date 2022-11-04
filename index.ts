01a2aae8c909c94ad9109626abd2b2e15f03856f
JoeHossam <86380699+JoeHossam@users.noreply.github.com>
2022-11-05 00:10:30 +0200

    added Doubly linked list to index.ts

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ import { BST as BinarySearchTree } from "./src/BST";
 import { SinglyLinkedList } from "./src/SinglyLinkedList";
 import { Stack } from "./src/Stack";
 import { Queue } from "./src/Queue";
+import { DoublyLinkedList } from "./src/DoublyLinkedList";
 
-export { BinarySearchTree, SinglyLinkedList, Stack, Queue };
+export { BinarySearchTree, SinglyLinkedList, Stack, Queue, DoublyLinkedList };
