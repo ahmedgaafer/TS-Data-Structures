669250bfd26e3d4712ab582c53da237aa1cab7a1
Ahmed gaafer <ahmeds.gaafer@gmail.com>
2022-11-12 14:35:36 +0200

    Description
    UPDATE:
     - tsconfig & package.json
     - index.ts location

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { BST as BinarySearchTree } from "./BST";
+import { SinglyLinkedList } from "./SinglyLinkedList";
+import { Stack } from "./Stack";
+import { Queue } from "./Queue";
+import { DoublyLinkedList } from "./DoublyLinkedList";
+import { Graph } from "./Graph";
+import { Heap } from "./Heap";
+
+export {
+	BinarySearchTree,
+	SinglyLinkedList,
+	Stack,
+	Queue,
+	DoublyLinkedList,
+	Graph,
+	Heap,
+};
